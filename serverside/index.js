44385c8e46798a30fed1f61fb2ebb95cb324909e
AjaxUberman <bariskayikcidev@gmail.com>
2024-06-22 00:01:27 +0300

    upd

--- serverside/index.js
+++ serverside/index.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "https://booking-bkayikci-fullstack.onrender.com",
+    origin: "http://localhost:3000",
   })
 );
 
