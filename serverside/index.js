21599cf344cd1fbd4e68a70bc1334eb388221ff7
AjaxUberman <bariskayikcidev@gmail.com>
2024-06-22 20:24:36 +0300

    fixes

--- serverside/index.js
+++ serverside/index.js
@@ -19,9 +19,20 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
-app.use(cors());
+
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(
+  cors({
+    origin: [
+      "http://bookingfullstack.netlify.app",
+      "https://bookingfullstack.netlify.app",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  })
+);
 
 mongoose.connect(process.env.MONGO_URL);
 
