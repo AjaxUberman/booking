482236d595d9cc6000060e4364d6224c22494755
AjaxUberman <bariskayikcidev@gmail.com>
2024-06-22 19:58:21 +0300

    vv4

--- serverside/index.js
+++ serverside/index.js
@@ -19,17 +19,7 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = process.env.JWT_SECRET;
 
 app.use(express.json());
-var whitelist = ["https://bookingfullstack.netlify.app"];
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
