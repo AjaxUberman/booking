c6deb6920dfaaeb648020c396f526940ed2b637d
AjaxUberman <bariskayikcidev@gmail.com>
2024-06-22 18:36:17 +0300

    fix

--- serverside/index.js
+++ serverside/index.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://bookingfullstack.netlify.app",
   })
 );
 
